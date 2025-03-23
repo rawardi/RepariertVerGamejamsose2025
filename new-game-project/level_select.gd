63d2d725d4142ffdcc418514ffb53b71b7e5dc12
Kunut0 <maxcunmun@gmail.com>
2025-03-23 11:47:22 +0100

    Level und so

---
+++ new-game-project/level_select.gd
@@ -0,0 +1,21 @@
+extends Control
+
+
+
+
+func _on_button_4_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Start.tscn")
+
+
+func _on_button_3_pressed() -> void:
+	BG_MUSIC._play_ingame_music()
+
+
+func _on_button_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/test_level3.tscn")
+	BG_MUSIC._play_ingame_music()
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/test_level4.tscn")
+	BG_MUSIC._play_ingame_music()
